6e5bde349d2c703205016386e900e246c91413f8
dhivyasri0126 <dhivyasri0126@gmail.com>
2025-07-28 10:05:57 +0530

    Update settings.py
    Removed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cxp!s*o66q@w@c-et(m-m^=&ty0w*h-@6%0)@p-#atqu-+=4j=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dhivyasri-blog.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
